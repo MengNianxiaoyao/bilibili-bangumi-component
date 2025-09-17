4726c95dbed2462758b2cda84292923b6b96e986
MengNianxiaoyao <2589141604@qq.com>
2025-09-17 11:13:15 +0800

    测试

---
+++ packages/api/src/edgeone.ts
@@ -0,0 +1,33 @@
+import { parseSearchParams } from '../../bilibili-bangumi-component/src/shared/utils'
+import { handler as bilibili } from './bilibili'
+import { handler as bgm } from './bgm'
+import { handleQuery } from './shared/utils'
+import { customHandler } from './shared'
+
+export const runtime = 'edge'
+
+export async function onRequestGet(request: Request) {
+  const url = new URL(request.url)
+  const query = handleQuery(parseSearchParams(url))
+
+  let customSource = {}
+  try {
+    customSource = customData
+  }
+  catch {
+
+  }
+
+  if (url.pathname.endsWith('bilibili'))
+    return await bilibili(query, process.env)
+  else if (url.pathname.endsWith('bgm'))
+    return await bgm(query, process.env)
+  else if (url.pathname.endsWith('custom'))
+    return customHandler(query, customSource)
+
+  return Response.json({
+    code: 404,
+    message: 'not found',
+    data: {},
+  }, { status: 404 })
+}
